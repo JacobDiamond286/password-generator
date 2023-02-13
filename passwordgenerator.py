5494e09f05ef00024026d05b03f3613dc372c502
Jacob Diamond <jacobdiamond286@outlook.com>
2023-02-13 14:24:50 -0700

    will this one work?

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,3 +1,14 @@
 import random
 
-chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()?"
+
+while 1:
+    password_len = int(input("What length would you like your password to be : "))
+    password_count = int(input("How many passwords would you like : "))
+    for x in range(0,password_count):
+        password = ""
+        for x in range(0,password_len):
+            password_char = random.choice(chars)
+            password = password + password_char
+        print(password)
+# adding to see if this commits
