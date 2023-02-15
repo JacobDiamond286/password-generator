7761623faa01b688dd0c8f6aa237aeb7ac4ef34e
Jacob Diamond <jacobdiamond286@outlook.com>
2023-02-15 12:45:43 -0700

    added nav bar and more views for each template

--- views.py
+++ views.py
@@ -5,3 +5,9 @@ views = Blueprint(__name__, 'views')
 @views.route("/")
 def home():
     return render_template("home.html")
+@views.route("/login")
+def login():
+    return render_template("login.html")
+@views.route("/signup")
+def signup():
+    return render_template("signup.html")
